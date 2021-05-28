54d3d2a64e6d7039473631351f4b4696c1b77e85
cganzon <crganzon@gmail.com>
2021-05-27 19:50:44 -0700

    displaying all breed names on a page

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+app.use(express.urlencoded({ extended: true }));
 const path = require("path");
 const axios = require("axios");
 
@@ -8,6 +9,7 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, '/public')));
 
+// Searches for random images
 app.get("/", (req, res) => {
   axios
     .get("https://api.thedogapi.com/v1/images/search?limit=5")
@@ -21,6 +23,19 @@ app.get("/", (req, res) => {
     });
 });
 
+app.get("/breeds", (req, res) => {
+  axios
+    .get('https://api.thedogapi.com/v1/breeds')
+    .then((response) => {
+      console.log(response.data);
+      let results = response.data;
+      res.render("breeds",  { results });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Listening at http://localhost:${port}`);
 });
