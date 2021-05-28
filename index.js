4b816f2758129170af93a9a25af3e8b3f6bee3ee
cganzon <crganzon@gmail.com>
2021-05-27 18:32:56 -0700

    connecting to api to display random images

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
+const path = require("path");
+const axios = require("axios");
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.get("/", (req, res) => {
+  axios
+    .get("https://api.thedogapi.com/v1/images/search?limit=5")
+    .then((response) => {
+      console.log(response.data);
+      let results = response.data;
+      res.render("home",  { results });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Example app listening at http://localhost:${port}`);
 });
